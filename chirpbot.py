11b6d19ce6be5525f30aa0344326584b88438fb2
Paul Goldschmidt <paul.goldschmidt@web.de>
2021-09-22 22:49:15 +0200

    added basic twitter bot capability

---
+++ chirpbot.py
@@ -0,0 +1,60 @@
+import tweepy
+import logging
+from config import create_api
+import time
+import os
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+def create_api():
+    with open('twittercredentials.json') as data_file:
+        data = json.load(data_file)
+
+    consumer_key = data['consumerKey']
+    consumer_secret = data['consumerSecret']
+    access_token = data['accessTokenKey']
+    access_token_secret = data['accessTokenSecret']
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth, wait_on_rate_limit=True, 
+        wait_on_rate_limit_notify=True)
+    try:
+        api.verify_credentials()
+    except Exception as e:
+        logger.error("Error creating API", exc_info=True)
+        raise e
+    logger.info("API created")
+    return api
+
+def check_mentions(api, keywords, since_id):
+    logger.info("Retrieving mentions")
+    new_since_id = since_id
+    for tweet in tweepy.Cursor(api.mentions_timeline,
+        since_id=since_id).items():
+        new_since_id = max(tweet.id, new_since_id)
+        if tweet.in_reply_to_status_id is not None:
+            continue
+        if any(keyword in tweet.text.lower() for keyword in keywords):
+            logger.info(f"Answering to {tweet.user.name}")
+
+            if not tweet.user.following:
+                tweet.user.follow()
+
+            api.update_status(
+                status="Please reach us via DM",
+                in_reply_to_status_id=tweet.id,
+            )
+    return new_since_id
+
+def main():
+    api = create_api()
+    since_id = 1
+    while True:
+        since_id = check_mentions(api, ["help", "support"], since_id)
+        logger.info("Waiting...")
+        time.sleep(60)
+
+if __name__ == "__main__":
+    main()
---
+++ predict_account.py
@@ -0,0 +1,78 @@
+import tweepy
+import sys
+import predict
+import statistics
+import json
+from datetime import datetime
+from datetime import timedelta
+from cleanuptext import clean_text
+
+# Import Twitter credentials from file
+with open('twittercredentials.json') as data_file:
+    data = json.load(data_file)
+
+consumer_key = data['consumerKey']
+consumer_secret = data['consumerSecret']
+access_key = data['accessTokenKey']
+access_secret = data['accessTokenSecret']
+
+
+def predict_party(twitter_handle):
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_key, access_secret)
+    api = tweepy.API(auth, wait_on_rate_limit=True,
+                     wait_on_rate_limit_notify=True)
+    # Limit number of Tweets being downloaded simultaneously
+    number_of_tweets = 100
+    toshort = 0
+    tweetsread = 0
+
+    # Start 60 days in the past
+    past = datetime.today() - timedelta(days=60)
+    predictions = {}
+    tweets = []
+    print("Serving request of downloading user \"" + str(twitter_handle) + "\"")
+    try:
+        for tweet in tweepy.Cursor(api.user_timeline, screen_name=twitter_handle).items(number_of_tweets):
+            # Create array of Tweet information: username, tweet id, date/time, text
+            tweettime = tweet.created_at
+            status = api.get_status(tweet.id_str, tweet_mode="extended")
+            # Check whether the criteria for analyzing tweets are met
+            if tweettime > past:
+                newtweettext = clean_text(str(status.full_text))
+                tweetlengh = str(newtweettext).count('')
+                # Ensure that the Tweet is longer than 4 characters
+                if tweetlengh > 4:
+                    tweets.append(newtweettext)
+                else:
+                    toshort = toshort + 1
+            else:
+                break
+
+    except:
+        print("Request failed.")
+        return dict({"success": False, "error": "Error while downloading Tweets, maybe 440?", "data": {}, "tweetsread": {0}})
+
+    for tweet in tweets:
+        prediction = predict.predict(tweet)
+        tweetsread = tweetsread + 1
+        for key, value in prediction.items():
+            if key not in predictions.keys():
+                predictions[key] = []
+            predictions[key].append(value)
+
+    for key, value in predictions.items():
+        predictions[key] = statistics.mean(value)
+    print("Success. Downloaded and analyzed in total " +
+          str(tweetsread) + " tweets. Exiting with return of results.")
+    return dict({"success": True, "error": {}, "data": predictions, "tweetsread": tweetsread})
+
+
+# Only executed when called manually
+if __name__ == '__main__':
+    result = predict_party(sys.argv[1])
+    if (result["success"] == True):
+        print(json.dumps(result))
+        exit(0)
+    # Print error
+    print(json.dumps(result["error"]))
