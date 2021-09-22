11653024bea59dfd0657623423f25308ea85fba1
Paul Goldschmidt <paul.goldschmidt@web.de>
2021-09-23 00:48:06 +0200

    see last commit

--- chirpbot.py
+++ chirpbot.py
@@ -62,9 +62,9 @@ def check_mentions(api, since_id):
         diegruenen = predictions["Bündnis 90/Die Grünen"] * 100
         piraten = predictions["Piratenpartei Deutschland"] * 100
         prettyresults = "CDU: " + str(round(cdu, 3)) + "% \n SPD: " + str(round(spd, 3)) + "% \n FDP: " + str(round(fdp, 3)) + "% \n Die Linke: " + str(round(dielinke, 3)) + "% \n Die Grünen: " + str(round(diegruenen, 3)) + "% \n Piratenpartei: " + str(round(piraten, 3)) + "% \n AFD: " + str(round(afd, 3)) + "%.\n\nInsgesamt wurden " + str(tweetsreadtotal) + " Tweets analysiert."
-        print(prettyresults)
+        tweetstatus = "@" + str(twitterrequester) + " Deine Tweets stimmen so viel mit den den folgenden Parteien überein: " + str(prettyresults)
         with open('temp.txt', 'w') as t:
-            t.write(tweetstatus = "@" + str(twitterrequester) + " Deine Tweets stimmen so viel mit den den folgenden Parteien überein: " + str(prettyresults))
+            t.write(tweetstatus)
             t.close()
 
         with open('temp.txt','r') as t:
