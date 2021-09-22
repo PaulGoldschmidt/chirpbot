f336fce4d46b7a2126e06b527c7054373d1a9659
Paul Goldschmidt <paul.goldschmidt@web.de>
2021-09-22 23:53:28 +0200

    added cleanup-text function

---
+++ cleanuptext.py
@@ -0,0 +1,60 @@
+# Cleanup text for better neural language processing for Chirpanalytica (USE ONLY FOR GERMAN MODELS)
+import sys
+import re
+import nltk
+from nltk.stem.snowball import SnowballStemmer
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+
+nltk.download('punkt')
+nltk.download('stopwords')
+
+stemmer = SnowballStemmer("german")
+stop_words = set(stopwords.words("german"))
+
+
+def clean_text(text, for_embedding=False):
+    """
+        - remove any html tags (< /br> often found)
+        - keep only ASCII + European Chars and whitespace, no digits
+        - remove single letter chars
+        - convert all whitespaces (tabs etc.) to single wspace
+        if not for embedding (but e.g. tdf-idf):
+        - all lowercase
+        - remove stopwords, punctuation and stemm
+    """
+    # For Twitter: cut out everything url-related
+    text = re.sub(r'http\S+', '', text)
+    RE_WSPACE = re.compile(r"\s+", re.IGNORECASE)
+    RE_TAGS = re.compile(r"<[^>]+>")
+    RE_ASCII = re.compile(r"[^A-Za-zÀ-ž ]", re.IGNORECASE)
+    RE_SINGLECHAR = re.compile(r"\b[A-Za-zÀ-ž]\b", re.IGNORECASE)
+    if for_embedding:
+        # Keep punctuation
+        RE_ASCII = re.compile(r"[^A-Za-zÀ-ž,.!? ]", re.IGNORECASE)
+        RE_SINGLECHAR = re.compile(r"\b[A-Za-zÀ-ž,.!?]\b", re.IGNORECASE)
+
+    text = re.sub(RE_TAGS, " ", text)
+    text = re.sub(RE_ASCII, " ", text)
+    text = re.sub(RE_SINGLECHAR, " ", text)
+    text = re.sub(RE_WSPACE, " ", text)
+
+    word_tokens = word_tokenize(text)
+    words_tokens_lower = [word.lower() for word in word_tokens]
+
+    if for_embedding:
+        # No stemming, lowering and punctuation / stop words removal
+        words_filtered = word_tokens
+    else:
+        words_filtered = [
+            stemmer.stem(word) for word in words_tokens_lower if word not in stop_words
+        ]
+
+    text_clean = " ".join(words_filtered)
+    return text_clean
+
+
+# Only executed when called manually
+if __name__ == '__main__':
+    result = clean_text(sys.argv[1].decode('utf-8'))
+    print(result)
