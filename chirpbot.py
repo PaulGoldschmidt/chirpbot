d732c1169b130f9795a1eccbeb44ff7eb271c57d
Paul Goldschmidt <paul.goldschmidt@web.de>
2021-09-23 15:00:51 +0200

    Working on fixing a duplication bug

--- chirpbot.py
+++ chirpbot.py
@@ -30,6 +30,7 @@ def create_api():
     return api
 
 def write_progress(progress_id):
+    print(progress_id)
     f = open("chirpbot_progress.txt", "r")
     if int(f.read()) > int(progress_id):
         f = open("chirpbot_progress.txt", "w")
@@ -42,10 +43,10 @@ def check_mentions(api, since_id):
     logger.info("Retrieving mentions")
     new_since_id = since_id
     for tweet in tweepy.Cursor(api.mentions_timeline, since_id=since_id).items():
+        print(tweet.id)
         new_since_id = write_progress(max(tweet.id, new_since_id))
         if tweet.in_reply_to_status_id is not None:
             continue
-        #if any(keyword in tweet.text.lower() for keyword in keywords):
         logger.info(f"Answering to {tweet.user.name}")
 
         if not tweet.user.following:
